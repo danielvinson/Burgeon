d0b09c9bd3684bfe07053e48c21d23225a1029a3
Daniel Vinson <danielvinson@gmail.com>
2018-03-16 19:41:05 +0000

    Update Registration page to match Login updates.

--- burgeon-client/src/containers/Register/Register.js
+++ burgeon-client/src/containers/Register/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import burgeonAPI from '../../api.js';
 
@@ -41,44 +42,55 @@ export default class Register extends Component {
   render() {
     return(
       <div className="Register">
-        <div className="registerContainer"> 
+        <div className="registerContainer">
+          <div className="contentTitle">Register</div>
           <form onSubmit={this.handleSubmitRegistration}>
-            <div className="formGroup">
-              <label>Email</label>
+            <div className="formGroup registerFormGroup">
+              <label className="formLabel">Email</label>
               <input
                 autoFocus
                 name="email"
                 type="email"
+                placeholder="Email Address"
                 value={this.state.email}
                 onChange={this.handleInputChange}
+                required
               />
             </div>
-            <div className="formGroup">
-              <label>Password</label>
+            <div className="formGroup registerFormGroup">
+              <label className="formLabel">Password</label>
               <input
                 name="password"
                 type="password"
+                placeholder="Password"
                 value={this.state.password}
                 onChange={this.handleInputChange}
+                required
               />
             </div>
-            <div className="formGroup">
-              <label>Verify Password</label>
+            <div className="formGroup registerFormGroup">
+              <label className="formLabel">Verify Password</label>
               <input
                 name="password2"
                 type="password"
+                placeholder="Verify Password"
                 value={this.state.password2}
                 onChange={this.handleInputChange}
+                required
               />
             </div>
-            <div className="formGroup">
+            <div className="formGroupInline registerFormGroupInline">
               <button
                 type="submit"
+                className="registerButton"
               >
                 Register
               </button>
             </div>
           </form>
+          <div className="regsiterLoginLink">
+            Already have an Account?  <Link to="/login">Log In</Link>.
+          </div>
         </div>
       </div>
     )
